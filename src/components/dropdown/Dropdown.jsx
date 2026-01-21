9964cb9b9284fcc17f161508b319e6419f6d8bd6
punit-fm <punit.suman@flairminds.com>
2026-01-21 12:27:02 +0530

    ui changes

--- src/components/dropdown/Dropdown.jsx
+++ src/components/dropdown/Dropdown.jsx
@@ -4,29 +4,29 @@ import learnMoreStyles from './Dropdown.module.css'
 import { useDispatch } from 'react-redux';
 import { toggleModal } from '../../redux/slices/visitorDetailsModalSlice';
 
-const Dropdown = ({chatbot}) => {
-    const [activeDropdown,setActiveDropDown]=useState(false)
-     const dispatch=useDispatch()
-    const toggleDropdown=()=>{
-        setActiveDropDown(!activeDropdown)
-      }
+const Dropdown = ({ chatbot }) => {
+  const [activeDropdown, setActiveDropDown] = useState(false)
+  const dispatch = useDispatch()
+  const toggleDropdown = () => {
+    setActiveDropDown(!activeDropdown)
+  }
   return (
     <>
-              <div className={learnMoreStyles.chatbot}>
-                <div className={`${learnMoreStyles.chatbot_top} ${learnMoreStyles.no_select} ${activeDropdown?learnMoreStyles.active_chatbot_top:learnMoreStyles.inactive_chatbot_top}`} onClick={()=>toggleDropdown()}> 
-                    <p>Try Chatbots</p>
-                    <RiArrowDropDownLine className={learnMoreStyles.dropdown_icon}/>
-                </div>
-                <div className={learnMoreStyles.dropdown_container}>
-                <div className={`${learnMoreStyles.chatbot_body} ${activeDropdown?learnMoreStyles.active_dropdown:learnMoreStyles.inactive_dropdown}`}>
-                  <ul>
-                    {chatbot.map((chat)=> 
-                        <li key={chat.id} className={learnMoreStyles.chatbot_list} onClick={()=>{window.open(chat.link)}} >{chat.text}</li>
-                    )}
-                  </ul>
-                </div>
-                </div>
-              </div>
+      <div className={learnMoreStyles.chatbot}>
+        <div className={`${learnMoreStyles.chatbot_top} ${learnMoreStyles.no_select} ${activeDropdown ? learnMoreStyles.active_chatbot_top : learnMoreStyles.inactive_chatbot_top}`} onClick={() => toggleDropdown()}>
+          Try Chatbots
+          <RiArrowDropDownLine className={learnMoreStyles.dropdown_icon} />
+        </div>
+        <div className={learnMoreStyles.dropdown_container}>
+          <div className={`${learnMoreStyles.chatbot_body} ${activeDropdown ? learnMoreStyles.active_dropdown : learnMoreStyles.inactive_dropdown}`}>
+            <ul>
+              {chatbot.map((chat) =>
+                <li key={chat.id} className={learnMoreStyles.chatbot_list} onClick={() => { window.open(chat.link) }} >{chat.text}</li>
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -8,22 +8,22 @@ import { setDemoLink, setShareLink, toggleDemoModal, toggleModal } from '../../r
 import { IoMdShare } from "react-icons/io";
 import { message, Carousel } from 'antd';
 import LearnMore from '../learnMore/LearnMore';
-import {motion} from 'motion/react'
+import { motion } from 'motion/react'
 import { MdOutlineVideoLibrary } from "react-icons/md";
 import { IoIosShareAlt } from "react-icons/io";
 
 const ProductCard = React.memo(({ prop }) => {
   const dispatch = useDispatch()
   const containerVariants = {
-    hidden: {y:20},
+    hidden: { y: 20 },
     visible: {
-      y:0,
+      y: 0,
       transition: {
         staggerChildren: 0.1,
       },
     },
   };
-  
+
   const itemVariants = {
     hidden: { opacity: 0, x: -30 },
     visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
@@ -32,29 +32,29 @@ const ProductCard = React.memo(({ prop }) => {
   const highlightPhrases = (text, phrases) => {
     if (!phrases || phrases.length === 0) return text;
     let parts = [text];
-  
+
     phrases.forEach(phrase => {
       const regex = new RegExp(`(${phrase})`, "gi");
       parts = parts.flatMap(part =>
         typeof part === "string"
           ? part.split(regex).map((p, i) =>
-              p.toLowerCase() === phrase.toLowerCase()
-                ? <strong key={i}>{p}</strong>
-                : p
-            )
+            p.toLowerCase() === phrase.toLowerCase()
+              ? <strong key={i}>{p}</strong>
+              : p
+          )
           : [part]
       );
     });
-  
+
     return parts;
   };
 
 
-	return (
-		<div className={productCardStyles.product_card} id={prop.id}>
-		<div className={productCardStyles.product_card_innner_container}>
-			<div className={productCardStyles.image_container}>
-			<div className={productCardStyles.product_card_image}>
+  return (
+    <div className={productCardStyles.product_card} id={prop.id}>
+      <div className={productCardStyles.product_card_innner_container}>
+        <div className={productCardStyles.image_container}>
+          <div className={productCardStyles.product_card_image}>
             {/* <Carousel className={productCardStyles.carousel} autoplay={{ dotDuration: true }} autoplaySpeed={2000}>
               <div>
                 <img src={prop?.image} alt="" style={{width:"100%",height:"100%"}} />
@@ -69,96 +69,96 @@ const ProductCard = React.memo(({ prop }) => {
                 <img src={prop?.image} alt="" style={{width:"100%",height:"100%"}} />
               </div>
             </Carousel> */}
-				{prop.image.endsWith('mp4')?<video src={`${prop.image}`} loop muted style={{width:'100%'}} autoPlay/>:<img src={prop?.image} alt="" style={{ width: "100%", height: "100%" }} />}
-			</div>
-			</div>
-			<motion.div
-				className={productCardStyles.details_container}
-				variants={containerVariants}
-				initial="hidden"
-				whileInView="visible"
-				viewport={{ once: true, amount: 0.3 }}
-			>
-			<motion.h1 variants={itemVariants}>
-				{prop?.name.toUpperCase()}
-			</motion.h1>
-			
-			<motion.div>
-				{prop.tags?.map((tag, i) => (
-					<motion.div variants={itemVariants} key={i} style={{marginRight:"5px", display:"inline-block", backgroundColor:"#dae2ffff", padding:"5px 10px", borderRadius:"5px"}}>
-						{tag}
-					</motion.div>
-				))}
-				{prop.techTags?.map((tag, i) => (
-					<motion.div variants={itemVariants} key={i} style={{marginRight:"5px", display:"inline-block", backgroundColor:"#ecececff", padding:"5px 10px", borderRadius:"5px"}}>
-						{tag}
-					</motion.div>
-				))}
-			</motion.div>
+            {prop.image.endsWith('mp4') ? <video src={`${prop.image}`} loop muted style={{ width: '100%' }} autoPlay /> : <img src={prop?.image} alt="" style={{ width: "100%", height: "100%" }} />}
+          </div>
+        </div>
+        <motion.div
+          className={productCardStyles.details_container}
+          variants={containerVariants}
+          initial="visible"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.3 }}
+        >
+          <motion.h1 variants={itemVariants}>
+            {prop?.name.toUpperCase()}
+          </motion.h1>
 
-			<motion.div style={{fontSize: '17px', lineHeight: '1.5', margin: '0 0.2rem'}}>
-				{prop?.detailsParagraphs?.map((paragraph, i) => (
-					<motion.p variants={itemVariants} key={i}>
-					{/* {paragraph} */}
-					{highlightPhrases(paragraph, prop?.highlight)}
-					</motion.p>
-				))}
+          <motion.div>
+            {prop.tags?.map((tag, i) => (
+              <motion.div variants={itemVariants} key={i} style={{ marginRight: "5px", display: "inline-block", backgroundColor: "#dae2ffff", padding: "5px 10px", borderRadius: "5px" }}>
+                {tag}
+              </motion.div>
+            ))}
+            {prop.techTags?.map((tag, i) => (
+              <motion.div variants={itemVariants} key={i} style={{ marginRight: "5px", display: "inline-block", backgroundColor: "#ecececff", padding: "5px 10px", borderRadius: "5px" }}>
+                {tag}
+              </motion.div>
+            ))}
+          </motion.div>
 
-				{/* <motion.h4 variants={itemVariants}>
+          <motion.div style={{ fontSize: '17px', lineHeight: '1.5', margin: '0 0.2rem' }}>
+            {prop?.detailsParagraphs?.map((paragraph, i) => (
+              <motion.p variants={itemVariants} key={i}>
+                {/* {paragraph} */}
+                {highlightPhrases(paragraph, prop?.highlight)}
+              </motion.p>
+            ))}
+
+            {/* <motion.h4 variants={itemVariants}>
 					{prop.subHeading}
 				</motion.h4> */}
 
-				<motion.ul
-					className={productCardStyles.bullets}
-					variants={containerVariants}
-				>
-					{prop?.list?.map((element, i) => (
-					<motion.li
-						// style={{ fontWeight: '600' }}
-						key={i}
-						variants={itemVariants}
-					>
-						{/* {element} */}
-						{highlightPhrases(element, prop?.highlight)}
-					</motion.li>
-					))}
-				</motion.ul>
-				<motion.p variants={itemVariants}>{highlightPhrases(prop?.cta, prop?.highlight)}</motion.p>
-			</motion.div>
-      <motion.div className={productCardStyles.options} variants={itemVariants}>
-        {prop.buttonText && (
-          <div className={productCardStyles.button_container}>
-            <button
-              onClick={() =>
-                window.open(prop.projectLink)
-              }
-              className={productCardStyles.product_card_button}
+            <motion.ul
+              className={productCardStyles.bullets}
+              variants={containerVariants}
             >
-              {prop.buttonText}
-              <FaArrowRight className={productCardStyles.icons}/>
-              {/* <img src={arrow} alt="arrow right" className={productCardStyles.arrow} /> */}
-            </button>
-          </div>
-        )}
+              {prop?.list?.map((element, i) => (
+                <motion.li
+                  // style={{ fontWeight: '600' }}
+                  key={i}
+                  variants={itemVariants}
+                >
+                  {/* {element} */}
+                  {highlightPhrases(element, prop?.highlight)}
+                </motion.li>
+              ))}
+            </motion.ul>
+            <motion.p variants={itemVariants}>{highlightPhrases(prop?.cta, prop?.highlight)}</motion.p>
+          </motion.div>
+          <motion.div className={productCardStyles.options} variants={itemVariants}>
+            {prop.buttonText && (
+              <div className={productCardStyles.button_container}>
+                <button
+                  onClick={() =>
+                    window.open(prop.projectLink)
+                  }
+                  className={productCardStyles.product_card_button}
+                >
+                  {prop.buttonText}
+                  <FaArrowRight className={productCardStyles.icons} />
+                  {/* <img src={arrow} alt="arrow right" className={productCardStyles.arrow} /> */}
+                </button>
+              </div>
+            )}
 
-        {prop.chatbot && (
-          <div className={productCardStyles.button_container}>
-            <Dropdown chatbot={prop.chatbot} />
-          </div>
-        )}
+            {prop.chatbot && (
+              <div className={productCardStyles.button_container}>
+                <Dropdown chatbot={prop.chatbot} />
+              </div>
+            )}
 
-        {prop?.demoButton?
-        <div className={productCardStyles.button_container} onClick={()=>{ dispatch(setDemoLink({ demoLink: prop.demoLink })); dispatch(toggleDemoModal()); }}>
-          <p className={productCardStyles.product_card_button}>View Demo<MdOutlineVideoLibrary style={{scale:"120%"}}/></p>
-        </div>:<div></div>}
-        <div className={productCardStyles.button_container} onClick={() => {dispatch(setShareLink({ shareName: prop.linkDisplay }))}}>
-          <p className={productCardStyles.product_card_button}>Share Link<IoIosShareAlt /></p>
-        </div>
-        
+            {prop?.demoButton ?
+              <div className={productCardStyles.button_container} onClick={() => { dispatch(setDemoLink({ demoLink: prop.demoLink })); dispatch(toggleDemoModal()); }}>
+                <p className={productCardStyles.product_card_button}>View Demo<MdOutlineVideoLibrary style={{ scale: "120%" }} /></p>
+              </div> : <div></div>}
+            <div className={productCardStyles.button_container} onClick={() => { dispatch(setShareLink({ shareName: prop.linkDisplay })) }}>
+              <p className={productCardStyles.product_card_button}>Share Link<IoIosShareAlt /></p>
+            </div>
 
-        {/* <LearnMore prop={prop} /> */}
-      </motion.div>
-</motion.div>
+
+            {/* <LearnMore prop={prop} /> */}
+          </motion.div>
+        </motion.div>
       </div>
       {(prop.upcomingFeatures && prop.upcomingFeatures.length > 0) && <>
         <div className={productCardStyles.upcoming_features}>
